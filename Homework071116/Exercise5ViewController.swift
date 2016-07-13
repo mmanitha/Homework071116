76681cf2b75b7a65f4b4c83c0d0ac560f99cd7c2
mmanitha <michael.manisa@gmail.com>
2016-07-13 00:36:06 -0400

    Completed BARE MINIMUM REQUIREMENTS

---
+++ Homework071116/Exercise5ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  Exercise5ViewController.swift
+//  Homework071116
+//
+//  Created by Michael Manisa on 7/13/16.
+//  Copyright © 2016 Michael Manisa. All rights reserved.
+//
+
+import UIKit
+
+class Exercise5ViewController: UIViewController, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tableView.dataSource = self
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Row \(indexPath.row)"
+        return cell
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
